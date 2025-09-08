45bf4c51faa792ec9e74c12cb4006cbb0cab047f
bashettyvarsha <bashettyvarsha@gmail.com>
2025-09-08 14:24:59 +0530

    code.js

---
+++ code.js
@@ -0,0 +1,12 @@
+let colorPickerContainer = document.getElementById("colorPickerContainer");
+let selectedColorHexCode = document.getElementById("selectedColorHexCode");
+
+function b1() {
+    colorPickerContainer.style.backgroundColor = "#e0e0e0";
+    selectedColorHexCode.textContent = "#e0e0e0";
+}
+
+function b2() {
+    colorPickerContainer.style.backgroundColor = "#6fcf97";
+    selectedColorHexCode.textContent = "#6fcf97";
+}
